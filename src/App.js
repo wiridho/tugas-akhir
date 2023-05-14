a1817b127f3c0261d817a50cf5a635ad083111b3
Farhan Hilmi <farhanhilmi077@gmail.com>
2023-05-14 10:56:23 +0700

    admin

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
-import Register from "./pages/authentication/Register";
-import RegisterRoles from "./pages/authentication/RegisterRoles";
-import Login from "./pages/authentication/Login";
-import VerifyRegister from "./pages/authentication/VerifyRegister";
-import VerifyLogin from "./pages/authentication/VerifyLogin";
-import Homepage from "./pages/Homepage/Homepage";
-import ForgotPassword from "./pages/authentication/ForgotPassword";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Register from './pages/authentication/Register';
+import RegisterRoles from './pages/authentication/RegisterRoles';
+import Login from './pages/authentication/Login';
+import VerifyRegister from './pages/authentication/VerifyRegister';
+import VerifyLogin from './pages/authentication/VerifyLogin';
+import Homepage from './pages/Homepage/Homepage';
+import ForgotPassword from './pages/authentication/ForgotPassword';
+import AdminDashboard from './pages/admin/dashboard';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Homepage />} />
 
-        <Route path="/register">
-          <Route index element={<RegisterRoles />} />
-          <Route path=":roles" element={<Register />} />
-        </Route>
-        <Route path="/login" element={<Login />} />
-        <Route path="/verifyregister" element={<VerifyRegister />} />
-        <Route path="/verifylogin" element={<VerifyLogin />} />
-        <Route path="/reset-password" element={<ForgotPassword />} />
-      </Routes>
-    </BrowserRouter>
-  );
+                <Route path="/register">
+                    <Route index element={<RegisterRoles />} />
+                    <Route path=":roles" element={<Register />} />
+                </Route>
+                <Route path="/login" element={<Login />} />
+                <Route path="/verifyregister" element={<VerifyRegister />} />
+                <Route path="/verifylogin" element={<VerifyLogin />} />
+                <Route path="/reset-password" element={<ForgotPassword />} />
+
+                {/* ADMIN ROUTES */}
+                <Route path="/admin/dashboard" element={<AdminDashboard />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/pages/admin/dashboard.js
@@ -0,0 +1,23 @@
+import Dashboard from '../../components/Admin/Layout';
+
+export default function AdminDashboard(second) {
+    return (
+        <>
+            <Dashboard />
+            {/* generate card with tailwindcss
+            <div className="flex flex-wrap">
+                <div className="w-full md:w-1/2 xl:w-1/3 p-3">
+                    <div className="bg-gray-900 border border-gray-800 rounded shadow p-2">
+                        <div className="flex flex-row items-center">
+                            HUEHEDUEDHUEHDUEDN
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="mt-2 mx-2 flex justify-center items-center">
+                <h1>INI ADMIN DASHBOARD</h1>
+            </div> */}
+        </>
+    );
+}
